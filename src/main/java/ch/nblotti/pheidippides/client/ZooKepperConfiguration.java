1343a79f2270c1ba61e6b7e6375074d5eb32dda7
nblotti <nblotti@gmail.com>
2021-07-29 17:08:32 +0200

    https://nblotti.atlassian.net/browse/PHEID-3

--- src/main/java/ch/nblotti/pheidippides/client/ZooKepperConfiguration.java
+++ src/main/java/ch/nblotti/pheidippides/client/ZooKepperConfiguration.java
@@ -1,21 +1,13 @@
 package ch.nblotti.pheidippides.client;
 
 import ch.nblotti.pheidippides.GeneratedExcludeJacocoTestCoverage;
-import ch.nblotti.pheidippides.kafka.KafkaConnectManager;
-import ch.nblotti.pheidippides.kafka.KafkaStreamManager;
-import ch.nblotti.pheidippides.kafka.PheidippidesTopology;
-import ch.nblotti.pheidippides.statemachine.EVENTS;
-import ch.nblotti.pheidippides.statemachine.STATES;
 import org.I0Itec.zkclient.ZkClient;
 import org.I0Itec.zkclient.exception.ZkMarshallingError;
 import org.I0Itec.zkclient.serialize.ZkSerializer;
-import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
-import org.springframework.statemachine.StateMachine;
-import org.springframework.web.client.RestTemplate;
 
 import java.nio.charset.StandardCharsets;
 
